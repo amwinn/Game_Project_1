895e34f7b9ccec15bdb27c190f4f74fbf4ad926b
amwinn <70451292+amwinn@users.noreply.github.com>
2026-03-17 03:53:14 -0500

    Overhaul Player Sprite Data and add new sprite set

--- data/player_spriteData.js
+++ data/player_spriteData.js
@@ -2,6 +2,42 @@
 //imported and directly referenced in src/Player.js, and GameLogic.js under the playerSpriteManager func
 export const spriteData_player = {
 
+
+    idle: {
+        left: "../Images/Entities/Entity_player/StoneAutomaton_walk_left_1.png",
+        right: "../Images/Entities/Entity_player/StoneAutomaton_walk_right_1.png"
+    },
+
+    walk: {
+        left: [
+        "../Images/Entities/Entity_player/StoneAutomaton_walk_left_1.png",
+        "../Images/Entities/Entity_player/StoneAutomaton_walk_left_2.png",
+        //"../Images/Entities/Entity_player/StoneAutomaton_walk_left_1.png",
+        "../Images/Entities/Entity_player/StoneAutomaton_walk_left_3.png"
+        ],
+
+        right: [
+        "../Images/Entities/Entity_player/StoneAutomaton_walk_right_1.png",
+        "../Images/Entities/Entity_player/StoneAutomaton_walk_right_2.png",
+        //"../Images/Entities/Entity_player/StoneAutomaton_walk_right_1.png",
+        "../Images/Entities/Entity_player/StoneAutomaton_walk_right_3.png"
+        ]
+
+    },
+    
+    
+    // default:"../Images/Entities/Entity_player/simple_wizard1_transparent_left.png",
+    // left: {
+    //     stance_1: "../Images/Entities/Entity_player/StoneAutomaton_walk_left_1.png",
+    //     stance_2: "../Images/Entities/Entity_player/StoneAutomaton_walk_left_2.png",
+    //     stance_3: "../Images/Entities/Entity_player/StoneAutomaton_walk_left_3.png"
+    // },
+    // right: {
+    //     stance_1: "../Images/Entities/Entity_player/StoneAutomaton_walk_right_1.png",
+    //     stance_2: "../Images/Entities/Entity_player/StoneAutomaton_walk_right_2.png",
+    //     stance_3: "../Images/Entities/Entity_player/StoneAutomaton_walk_right_3.png"
+    // }
+
     stance_1: {
         left: "../Images/Entities/Entity_player/simple_wizard1_transparent_left.png",
         right: "../Images/Entities/Entity_player/simple_wizard1_transparent.png",
