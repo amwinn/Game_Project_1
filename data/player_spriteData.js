febb9ebe168a7cfc32cd060f335441062a184bf0
amwinn <70451292+amwinn@users.noreply.github.com>
2026-03-18 02:37:14 -0500

    Remove prototype implementation and experimental code

--- data/player_spriteData.js
+++ data/player_spriteData.js
@@ -1,5 +1,5 @@
 
-//imported and directly referenced in src/Player.js, and GameLogic.js under the playerSpriteManager func
+//imported and directly referenced in src/Player.js, and GameLogic.js under the playerSpriteManager function
 export const spriteData_player = {
 
 
@@ -12,51 +12,24 @@ export const spriteData_player = {
         left: [
         "../Images/Entities/Entity_player/StoneAutomaton_walk_left_1.png",
         "../Images/Entities/Entity_player/StoneAutomaton_walk_left_2.png",
-        //"../Images/Entities/Entity_player/StoneAutomaton_walk_left_1.png",
+        "../Images/Entities/Entity_player/StoneAutomaton_walk_left_1.png",
         "../Images/Entities/Entity_player/StoneAutomaton_walk_left_3.png"
         ],
 
         right: [
         "../Images/Entities/Entity_player/StoneAutomaton_walk_right_1.png",
         "../Images/Entities/Entity_player/StoneAutomaton_walk_right_2.png",
-        //"../Images/Entities/Entity_player/StoneAutomaton_walk_right_1.png",
+        "../Images/Entities/Entity_player/StoneAutomaton_walk_right_1.png",
         "../Images/Entities/Entity_player/StoneAutomaton_walk_right_3.png"
         ]
-
     },
     
     
-    // default:"../Images/Entities/Entity_player/simple_wizard1_transparent_left.png",
-    // left: {
-    //     stance_1: "../Images/Entities/Entity_player/StoneAutomaton_walk_left_1.png",
-    //     stance_2: "../Images/Entities/Entity_player/StoneAutomaton_walk_left_2.png",
-    //     stance_3: "../Images/Entities/Entity_player/StoneAutomaton_walk_left_3.png"
-    // },
-    // right: {
-    //     stance_1: "../Images/Entities/Entity_player/StoneAutomaton_walk_right_1.png",
-    //     stance_2: "../Images/Entities/Entity_player/StoneAutomaton_walk_right_2.png",
-    //     stance_3: "../Images/Entities/Entity_player/StoneAutomaton_walk_right_3.png"
+    // stance_1: {
+    //     left: "../Images/Entities/Entity_player/simple_wizard1_transparent_left.png",
+    //     right: "../Images/Entities/Entity_player/simple_wizard1_transparent.png",
+    //     default: "../Images/Entities/Entity_player/simple_wizard1_transparent.png",
     // }
 
-    stance_1: {
-        left: "../Images/Entities/Entity_player/simple_wizard1_transparent_left.png",
-        right: "../Images/Entities/Entity_player/simple_wizard1_transparent.png",
-        default: "../Images/Entities/Entity_player/simple_wizard1_transparent.png",
-    },
-
-    // stance_2: {
-    //     left:,
-    //     right:,
-    //     default:
-    // }
-
-    // stance_3: {
-    //     left:,
-    //     right:,
-    //     default:
-    // }
-
-    // change if dont like but basically stance 1 will be the default stand-still pose, stance 2 will be the right knee forward pose, and
-    // stance 3 will be the left knee forward pose. im intending to place the stance 1 between each of the other poses to mimic transition of legs
 }
 
