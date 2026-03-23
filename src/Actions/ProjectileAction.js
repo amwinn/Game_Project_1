5defbbac6c7dd4605caf03a7b796266c9bb9e4af
amwinn <70451292+amwinn@users.noreply.github.com>
2026-03-22 22:18:26 -0500

    Change camera adjustment in process function

--- src/Actions/ProjectileAction.js
+++ src/Actions/ProjectileAction.js
@@ -13,11 +13,11 @@ export default class ProjectileAction extends Action {
 //important note regarding the role of the camera: 
 //w/o the camera the player is shooting the projectiles at the monitor's position (the starting viewport at 0,0), while the player is technically far off the screen by this point
 //not 100% happy with this yet, due to camera coupling, harcodinged numbers for clientx/y + number, nad for positionx/y + number
-    process(source, destination, camera) {
-        const x = (destination.clientX-20) - (source.position.x + 32);
-        const y = (destination.clientY-20) - (source.position.y + 50);
-        const angle = Math.atan2(y + camera.y, x + camera.x);
-        const velocity = {x: Math.cos(angle)*3, y: Math.sin(angle)*3};
+    process(source, target, camera) {
+        const x = ((target.clientX+camera.x)-20) - (source.position.x + 32);
+        const y = ((target.clientY+camera.y)-20) - (source.position.y + 50);
+        const angle = Math.atan2(y, x); // removed (y + camera.y, x + camera.x) and moved to const x and y values above; revert if any bug appears
+        const velocity = {x: Math.cos(angle)*5, y: Math.sin(angle)*5};
         const size = new Size(30,30);
         const position = new Position(source.position.x + 32, source.position.y +50);
         projectilesArray.push(new Projectile(Projectile.playerProjectile, position, size, velocity));
