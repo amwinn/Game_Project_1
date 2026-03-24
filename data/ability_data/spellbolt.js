f9f29949db25c3b68b4a7399a1628a5f7e80a9c4
amwinn <70451292+amwinn@users.noreply.github.com>
2026-03-24 03:09:29 -0500

    Add nascent spell data

---
+++ data/ability_data/spellbolt.js
@@ -0,0 +1,19 @@
+//WIP, format not finalized
+//id may be redundant
+//need the array [] for required and effect because ill be feeding into a forEach (unless?), form will mostly all be type, speed, scale - could even add cooldown into there?
+
+const spellbolt = {
+    id:"spellbolt",
+    cooldown: 300,
+    required: [
+        {type: "mana", amount: 10}
+    ],
+    form: {
+        type: "projectile",
+        speed: 5,
+        scale: 1
+    },
+    effect:[{type: "damage", amount: 20}
+    ]
+
+}
