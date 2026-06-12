ad40eb5bafc8e0c28892961b12b92d9b05f59ae8
amwinn <70451292+amwinn@users.noreply.github.com>
2026-06-11 23:35:00 -0500

    Deprecate old stance data

--- data/entity_data/entity_render.js
+++ data/entity_data/entity_render.js
@@ -27,11 +27,11 @@ export const CREATURE_RENDER = {
 
         //BELOW MUST BE REPLACES BEFORE PLUGGING CREATURE_RENDER INTO IMAGE LOADING FUNCTIONS, OR ELSE WILL ITERATE THROUGH STANCE_1 WHICH ISNT FORMATTED FOR THE NEW SYSTEM
         //deprecate below section once fully replaced
-        stance_1: {
-            left: "Images/Entities/Entity_forestSprite/forestSprite_left.png",
-            right: "Images/Entities/Entity_forestSprite/forestSprite_right.png",
-            default: "Images/Entities/Entity_forestSprite/forestSprite_1.png"
-        }
+        // stance_1: {
+        //     left: "Images/Entities/Entity_forestSprite/forestSprite_left.png",
+        //     right: "Images/Entities/Entity_forestSprite/forestSprite_right.png",
+        //     default: "Images/Entities/Entity_forestSprite/forestSprite_1.png"
+        // }
     },
     
     //technically they are both forest sprites but im making the mage a temp diff race until the entity role strategy is fully implemented
@@ -58,11 +58,11 @@ export const CREATURE_RENDER = {
 
         //BELOW MUST BE REPLACES BEFORE PLUGGING CREATURE_RENDER INTO IMAGE LOADING FUNCTIONS, OR ELSE WILL ITERATE THROUGH STANCE_1 WHICH ISNT FORMATTED FOR THE NEW SYSTEM
         //deprecate below section once fully replaced
-        stance_1: {
-            left: "Images/Entities/Entity_forestSpriteMage/forestSpriteMage_left.png",
-            right: "Images/Entities/Entity_forestSpriteMage/forestSpriteMage_right.png",
-            default: "Images/Entities/Entity_forestSpriteMage/forestSpriteMage_1.png"
-        }
+        // stance_1: {
+        //     left: "Images/Entities/Entity_forestSpriteMage/forestSpriteMage_left.png",
+        //     right: "Images/Entities/Entity_forestSpriteMage/forestSpriteMage_right.png",
+        //     default: "Images/Entities/Entity_forestSpriteMage/forestSpriteMage_1.png"
+        // }
     }
 }
 
