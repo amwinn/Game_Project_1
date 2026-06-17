55debe589706da1b8f7b1bc5fb68d6087de31f6c
amwinn <70451292+amwinn@users.noreply.github.com>
2026-06-16 21:09:35 -0500

    Add rough-draft pseudo code framework

--- src/AssetRegistry.js
+++ src/AssetRegistry.js
@@ -25,7 +25,7 @@ export function loadEntityRenders(data) {
 
 export default class AssetRegistry {
     constructor() {
-
+        this.registry = [];
     }
 
     //registerImageObject() something like this could automatically create an image/sprite() obejct?
@@ -68,7 +68,16 @@ export default class AssetRegistry {
 
 }
 
-//updated brainstorm:
+
+
+//updated brainstorm 3:
+//now leaning towards multiple instances of AssetRegistry, such as biomeAssetRegistry, globalAssetRegistry, etc.
+//does AssetRegistry class have a list of all data files that it would possibly need at runtime?
+//for example, has all biome data, so const uncachedRegistry= {swamp: {SWAMP_OBJECT_RENDER, SKELETON_RENDER}, forest: {FOREST_OBJECT_RENDER, CREATURE_RENDER}}
+//under constructor this.registry = [] or something akin to this
+//then biomeAssetRegistry.changeBiome(load, unload) would yield biomeAssetRegistry.changeBiome(uncachedRegistry[new_biome], biomeAssetRegistry.registry)
+
+//updated brainstorm 2:
 //will have one single instance of AssetRegistry
 //will do assetRegistry.loadImageGroup(Swamp) for example
 //loadImageGroup() function could possibly call two other functions, such as loadGameObjectAssets(FOREST_OBJECTS from gameObject_render.js) and loadEntityAssets(FOREST_CREATER from entity_render.js)
@@ -80,7 +89,7 @@ export default class AssetRegistry {
 
 
 
-//general brainstorm
+//general brainstorm 1
 //will need to have a function that acts as a factory of sorts, creating image objects with sprite paths
 //these objects can remain dormant and not used until attached to player.sprite, entity.sprite etc., during spriteManagement due to movement etc.
 //some research shows that there could be a static obejct that will hold all the image objects, not sure if i like that though
